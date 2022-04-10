078d221e3f41a71d1bf62ae04740685db2675995
kk-2005 <97383050+kk-2005@users.noreply.github.com>
2022-04-10 15:42:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,81 @@
+var towerImg, tower;
+var doorImg, door, doorsGroup;
+var climberImg, climber, climbersGroup;
+var ghost, ghostImg;
+var invisibleBlockGroup, invisibleBlock;
+var gameState = "play"
+
+function preload(){
+  towerImg = loadImage("tower.png");
+  doorImg = loadImage("door.png");
+  climberImg = loadImage("climber.png");
+  ghostImg = loadImage("ghost-standing.png");
+  spookySound = loadSound("spooky.wav");
+  doorsGroup=new Group();
+  climbersGroup=new Group();
+  invisibleBlockGroup=new Group();
+}
+
+function setup() {
+  createCanvas(600, 600);
+  tower = createSprite(300,300);
+  tower.addImage("tower",towerImg);
+  tower.velocityY = 1;
+  ghost=createSprite(200,200,50,50);
+  ghost.scale=0.3;
+  ghost.addImage("ghost",ghostImg);
+  
+}
+
+function draw() {
+  background(200);
+  
+  if(tower.y > 400){
+      tower.y = 300
+    }
+  if(keyDown("left_arrow")){
+    ghost.x=ghost.x-3;
+  }
+  if(keyDown("right_arrow")){
+    ghost.x=ghost.x+3;
+  }
+  if(keyDown("space")){
+    ghost.velocityY=-5;
+  }
+  ghost.velocityY=ghost.velocityY+0.8;
+  if(climbersGroup.isTouching(ghost)){
+    ghost.velocityY=0;
+  }
+  if(invisibleBlockGroup.isTouching(ghost) || ghost.y>600){
+    ghost.destroy();
+    gameState="end";
+  }
+  spawnDoors();
+  drawSprite();
+}
+function spawnDoors(){
+  if(frameCount%200===0){
+    var door=createSprite(200,-50);
+    door.addImage(doorImg);
+    var climber=createSprite(200,10);
+    climber.addImage(climberImg);
+    invisibleBlock=createSprite(200,15);
+    invisibleBlock.width=climber.width;
+    invisibleBlock.height=2;
+    door.x=Math.round(random(120,400));
+    door.velocityY=1;
+    climber.x=door.x;
+    invisibleBlock.x=door.x;
+    climber.velocityY=1;
+    invisibleBlock.velocityY=1;
+    ghost.depth=door.depth;
+    ghost.depth+=1;
+    door.lifetime=709;
+    doorsGroup.add(door);
+    climber.lifetime=709;
+    invisibleBlock.lifetime=709;
+    climbersGroup.add(climber);
+    invisibleBlockGroup.add(invisibleBlock);
+  }
+}
+
